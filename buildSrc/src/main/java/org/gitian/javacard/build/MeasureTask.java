218eaac9094bf0a496b7eb17d7609c8f8621bdd3
Devrandom <miron@basezero.com>
2019-10-06 19:28:26 +0200

    measure task

--- buildSrc/src/main/java/org/gitian/javacard/build/MeasureTask.java
+++ buildSrc/src/main/java/org/gitian/javacard/build/MeasureTask.java
@@ -12,19 +12,21 @@ import javax.smartcardio.CardException;
 import java.io.IOException;
 import java.util.Arrays;
 
+import static java.lang.System.currentTimeMillis;
 import static pro.javacard.gp.GPException.check;
 
-public class PingTask extends DefaultTask {
+public class MeasureTask extends DefaultTask {
+  private static final int ITERATIONS = 10;
   private final Logger logger;
   private APDUBIBO channel;
   private Card card;
 
-  public PingTask() {
+  public MeasureTask() {
     logger = getLogger();
   }
 
   @TaskAction
-  public void ping() {
+  public void measure() {
     card = new Card();
     try {
       card.open();
@@ -39,6 +41,8 @@ public class PingTask extends DefaultTask {
       select();
       logger.info("Ping");
       do_ping();
+      logger.info("EC");
+      do_ec();
       logger.info("Success");
     } catch (IOException e) {
       throw new GradleException("I/O error", e);
@@ -57,5 +61,24 @@ public class PingTask extends DefaultTask {
     if (!Arrays.equals(result.getData(), "Hello".getBytes())) {
       throw new IOException("did not get Hello");
     }
+  }
+
+  private void do_ec() throws IOException {
+    time("sign", 0x01);
+    time("pub", 0x02);
+    time("derive priv", 0x03);
+  }
+
+  private void time(String name, int task) throws IOException {
+    long startTime = currentTimeMillis();
+    CommandAPDU cmd = new CommandAPDU(0x80, task, 0, 0, new byte[0]);
+    check(channel.transmit(cmd));
+    long warmTime = currentTimeMillis();
+    cmd = new CommandAPDU(0x80, task, ITERATIONS, 0, new byte[0]);
+    check(channel.transmit(cmd));
+    long fullTime = currentTimeMillis();
+
+    logger.info("{} warm {}ms loop {}ms", name, warmTime - startTime, fullTime - warmTime);
+    logger.info("{} in average {}ms", name, (fullTime - warmTime - (warmTime - startTime)) / ITERATIONS);
   }
 }
